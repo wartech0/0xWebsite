df46ecf9d18c511fb2a224934dbcff3d6e55535f
wartech0 <wartech0@hotmail.com>
2014-10-08 11:16:59 -0500

    Initial Commit

--- public/js/app.js
+++ public/js/app.js
@@ -10,16 +10,12 @@ angular.module('myApp', [
 ]).
 config(function ($routeProvider, $locationProvider) {
   $routeProvider.
-    when('/view1', {
-      templateUrl: 'partials/partial1',
-      controller: 'MyCtrl1'
-    }).
-    when('/view2', {
-      templateUrl: 'partials/partial2',
-      controller: 'MyCtrl2'
+    when('/', {
+      templateUrl: 'frontend/home.html',
+      controller: 'IndexController'
     }).
     otherwise({
-      redirectTo: '/view1'
+      redirectTo: '/'
     });
 
   $locationProvider.html5Mode(true);
